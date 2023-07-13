c3d0f2a7991176c82ff1bf693343b2d27d5343b4
Ayushree Shende <ayushreeshede03@gmail.com>
2023-07-13 18:09:07 +0530

    programming

---
+++ c  programing/addition.c3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+	int num1,num2,num3,result;
+	printf("enter num1");
+	scanf("%d",&num1);
+	printf("enter num2");
+	scanf("%d",&num2);
+	printf("enter num3");
+	scanf("%d,&num3");
+	result=num1+num2+num3;
+	printf("addition=%d",result);
+}
---
+++ c  programing/cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int num,cube;
+	printf("enter num");
+	scanf("%d",&num);
+	cube=num*num*num;
+	printf("cube=%d",cube);
+	
+}
