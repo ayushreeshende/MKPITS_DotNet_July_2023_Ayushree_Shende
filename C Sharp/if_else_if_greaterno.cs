8bd2457289d423f8a3a925770845dd1d3715e28f
ayushreeshende <ayushreeshede03@gmail.com>
2023-10-13 22:48:02 +0530

    csharp

---
+++ C Sharp/if_else_if_greaterno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelsegreater
+{
+    internal class Program
+    {
+       public static void Main()
+        {
+            int num1, num2;
+            Console.WriteLine("enter number 1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2)
+                Console.WriteLine("num 1 is greater");
+            else
+                Console.WriteLine("num2 is greater");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C Sharp/pre_increament.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace increament
+{
+    internal class Program
+    {
+       public static void Main()
+        {
+            int i = 0;
+            int j = ++i;
+            Console.WriteLine("value of i :" + i + "\n value of j:" + j);
+            Console.ReadKey();
+        }
+    }
+}
