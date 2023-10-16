1a0f53c3f0984823c27782b89857993bdd341a87
ayushreeshende <ayushreeshede03@gmail.com>
2023-10-16 23:03:07 +0530

    csharp

---
+++ C Sharp/operator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Operator2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            char op;
+            Console.Write("Enter Num1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Num 2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter op(+,-,*,/)");
+            op = Convert.ToChar(Console.ReadLine());
+            if (op == '+')
+            {
+                sum = num1 + num2;
+                Console.Write("Result is " + sum);
+            }
+            else if (op == '-')
+            {
+                sum = num1 - num2;
+                Console.Write("Result is " + sum);
+            }
+            else if (op == '*')
+            {
+                sum = num1 * num2;
+                Console.Write("Result is " + sum);
+            }
+            else if (op == '/')
+            {
+                sum = num1 / num2;
+                Console.Write("Result is " + sum);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
