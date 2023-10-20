9d9ac93a6a27eead22ebe2d81eb157ca3f6a23f8
ayushreeshende <ayushreeshede03@gmail.com>
2023-10-20 22:47:14 +0530

    csharp

---
+++ C Sharp/sqrnum/sqrnum/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace sqrnum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int num = Convert.ToInt32(textBox1.Text);
+            int square = num * num;
+            label2.Text = "square " + square;
+
+
+        }
+    }
+}
+
