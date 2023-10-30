e363019795cf54150a490275f92505d4d3a2ce47
ayushreeshende <ayushreeshede03@gmail.com>
2023-10-30 22:36:45 +0530

    csharp

---
+++ C Sharp/array_sort_method.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_sort_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 1, 5, 3 };
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+      
