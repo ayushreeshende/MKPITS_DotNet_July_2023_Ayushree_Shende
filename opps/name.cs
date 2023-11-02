937f9254b8750a707a6c78fcef7dc9604ec19761
ayushreeshende <ayushreeshede03@gmail.com>
2023-11-02 22:22:13 +0530

    oops

---
+++ opps/name.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace name
+{
+    internal class Program
+    {
+        static void display(string name)
+        {
+            int name;
+            {
+                Console.WriteLine("Welcome friend " + " name +"!");
+            
+           
+                Console.WriteLine("Have a nice day!");
+            }
+            static void Main()
+            {
+                string name;
+                Console.Write("enter name ");
+                name = (console.Readline());
+                display(name);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
+
