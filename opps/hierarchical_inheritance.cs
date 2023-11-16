450620667ed7d581f2f239aa66e5a48ca0766462
ayushreeshende <ayushreeshede03@gmail.com>
2023-11-17 00:33:11 +0530

    oops

---
+++ opps/hierarchical_inheritance.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hierarchical_inheritance
+{
+    class person
+    {
+     public   string name;
+       public string address;
+    }
+    class student:person
+    {
+        public int rno;
+     public   int marks;
+    }
+    class employee:person
+    {
+       public int salary;
+     public   string designation;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            student s1 = new student();
+            s1.name = "sumit";
+            s1.address = "mumbai";
+            s1.rno = 123;
+            s1.marks = 450;
+            Console.WriteLine("-----student details----");
+            Console.WriteLine("name:" +s1.name);
+            Console.WriteLine("address:" + s1.address);
+            Console.WriteLine("rno:" + s1.rno);
+            Console.WriteLine("marks:" + s1.marks);
+        
+        employee emp = new employee();
+            emp.name="sumit";
+            emp.address="mumbai";
+            emp.salary=45670;
+            emp.designation="clerk";
+
+            Console.WriteLine("----emp details-----");
+            Console.WriteLine("name:" + emp.name);
+            Console.WriteLine("address:" + emp.address);
+            Console.WriteLine("salary:" + emp.salary);
+            Console.WriteLine("designation:" + emp.designation);
+            Console.ReadKey();
+        }
+    }
+    }
+   
---
+++ opps/multiple_inheritance_person_employee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiple_inheritnce_person_employee
+{
+    class person
+    {
+        string name;
+        string address;
+        public void getpersondata(string name,string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public void  displaypersondata()
+        {
+            Console.WriteLine("name:" + name);
+            Console.WriteLine("address:" + address);
+        }
+    }
+    class employee:person
+    {
+        int salary;
+        string designation;
+        public void getemployeedata(int salary,string designation)
+        {
+            this.salary = salary;
+            this.designation = designation;
+        }
+        public void displayemployeedata()
+        {
+            Console.WriteLine("salary:" + salary);
+            Console.WriteLine("designation:" + designation);
+        }
+    }
+    class parttimeemployee:employee
+    {
+        int no_of_hours;
+        public void getparttimeemployeedata(int no_of_hours)
+        {
+            this.no_of_hours=no_of_hours;
+        }
+        public void displayparttimeemployeedata()
+        {
+            Console.WriteLine("no_ of _hours:" + no_of_hours);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            parttimeemployee p1 = new parttimeemployee();
+            p1.getemployeedata(4500, "manager");
+            p1.getpersondata("prunit", "kolkatta");
+            p1.getparttimeemployeedata(9);
+            p1.displayemployeedata();
+            p1.displayparttimeemployeedata();
+            p1.displaypersondata();
+            Console.ReadKey();
+        }
+    }
+}
