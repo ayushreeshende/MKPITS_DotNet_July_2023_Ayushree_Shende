2e5b292638a244e2c4f769fa82972070f5cb0dd9
ayushreeshende <ayushreeshede03@gmail.com>
2023-11-24 23:48:11 +0530

    oops

---
+++ opps/hashtable.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Diagnostics.Eventing.Reader;
+
+namespace hashtable
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+
+            ht.Add("123", "amit");
+            ht.Add("001", "nora");
+            ht.Add("004", "hritik");
+            ht.Add("009", "rohit");
+            ht.Add("345", "kamlesh");
+            if (ht.ContainsValue(" sumit"))
+            {
+
+
+                Console.WriteLine("the student name already in the list");
+            }
+            else
+
+            {
+                ht.Add("008", "sumit");
+            }
+            
+
+            
+            
+              ICollection key= ht.Keys;
+            
+                foreach(string s in key)
+                {
+
+                    Console.WriteLine(s + ": " + ht[s]);
+                }
+              
+                    Console.ReadKey();
+                }
+            }
+                    
+        }
+    
+
---
+++ opps/sorted_list.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Security.Cryptography;
+using System.Diagnostics.Eventing.Reader;
+
+namespace sorted_list_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sl = new SortedList();
+            sl.Add("102", "khush");
+            sl.Add("103", "monty");
+            sl.Add("104", "tinu");
+            sl.Add("105", "mahesh");
+
+            if (sl.ContainsValue("hardik"))
+            {
+
+
+                Console.WriteLine("the student have already in the list");
+            }
+            else
+            {
+                sl.Add("008", "ashish");
+            }
+            ICollection key = sl.Keys;
+            foreach (string s in key)
+            {
+
+                Console.WriteLine(s + ": " + sl[s]);
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
+           
+    
+   
