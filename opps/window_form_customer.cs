e13cf77c8570141210d95b97a751ebbe7f0fe0b0
ayushreeshende <ayushreeshede03@gmail.com>
2023-11-29 22:24:04 +0530

    oops

---
+++ opps/window_form_customer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_customer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Dictionary<int, customer> cust = new Dictionary<int, customer>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            customer c1 = new customer(Convert.ToInt32(textBox1.Text), textBox3.Text);
+            customer c2 = new customer(Convert.ToInt32(textBox2.Text), textBox4.Text);
+            cust.Add(c1.Id, c1);
+            cust.Add(c2.Id, c2);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<int, customer> kv in cust)
+            {
+                sb.Append("cust id " + kv.Key + " customer name " + kv.Value.name + "\n");
+            }
+            label5.Text = sb.ToString();
+        }
+
+    }
+}
