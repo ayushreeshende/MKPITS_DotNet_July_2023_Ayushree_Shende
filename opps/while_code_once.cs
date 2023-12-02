81f045eff5d5cfd47aae858800e77453bfbbe6e8
ayushreeshende <ayushreeshede03@gmail.com>
2023-12-02 22:06:36 +0530

    oops

---
+++ opps/while_code_once.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_codeonce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 11;
+            do
+            {
+                Console.WriteLine("num={0}", num);
+                num++;
+            } while (num <= 5);
+            Console.WriteLine("bye");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ opps/windows_forms_property.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_property
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(textBox1.Text);
+
+            int sqr = num * num;
+            label3.Text = "square:" + sqr;
+
+        }
+    }
+}
