9348f4a3b5f3e885dce8aa841e85fcbed4813779
ayushreeshende <ayushreeshede03@gmail.com>
2023-12-07 22:51:06 +0530

    oops

---
+++ opps/window_form_toolbar.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_scroll_bar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("name:" + textBox1.Text +"\n");
+            sb.Append("dob:" +textBox2.Text+"\n");
+            sb.Append("address:" + textBox3.Text + "\n");
+            sb.Append("email id:" + textBox4.Text + "\n");
+            sb.Append("city:" + textBox5.Text + "\n");
+            sb.Append("qualification:" + textBox6.Text + "\n");
+            sb.Append("nationality:" + textBox7.Text + "\n");
+            label8.Text = sb.ToString();
+        }
+
+        private void textBox16_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("first name:" + textBox8.Text + "\n");
+            sb.Append("surname:" + textBox14.Text + "\n");
+            sb.Append("father name:" + textBox9.Text + "\n");
+            sb.Append("mother name:" + textBox10.Text + "\n");
+            sb.Append("father occupation:" + textBox11.Text + "\n");
+            sb.Append("mother occupation:" + textBox15 + "\n");
+            sb.Append("dob:" + textBox12.Text + "\n");
+            sb.Append("qualification:" + textBox16 + "\n");
+            string gender = null;
+            if (radioButton1.Checked)
+                gender = radioButton1.Text;
+            else if (radioButton2.Checked)
+                gender = radioButton2.Text;
+            sb.Append("gender:" + gender);
+            label19.Text = sb.ToString();
+        }
+        
+    }
+}
---
+++ opps/windowsform_menu_toolstrip.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_menu_toolstrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string fn = openFileDialog1.FileName;
+                MessageBox.Show(fn);
+                if (fn.EndsWith("rtf"))
+                {
+                    richTextBox1.LoadFile(fn);
+
+                }
+                else
+                {
+                    MessageBox.Show("cannot open this file");
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+
+            {
+                string fn = saveFileDialog1.FileName;
+                MessageBox.Show(fn);
+                if (fn.EndsWith("rtf"))
+                {
+                    richTextBox1.SaveFile(saveFileDialog1.FileName);
+                    MessageBox.Show("file saved successfully");
+                }
+                else
+                {
+                    MessageBox.Show("cannot save file of this format");
+                }
+            }
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionColor = colorDialog1.Color;
+            }
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Copy();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Cut();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Paste();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Undo();
+        }
+
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            if (fontDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.SelectionFont = fontDialog1.Font;
+
+
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+    }
+}
+
