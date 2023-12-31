54367703ba862764ef8ee611433ef2a86dd31953
ayushreeshende <ayushreeshede03@gmail.com>
2023-12-31 22:06:17 +0530

    c sharp

---
+++ C Sharp/window_array.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms_array
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int num[] = new int[5];
+            int i = 0;
+        int sum = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(i<5)
+            {
+num i=Convert.ToInt32(textBox1.Text)
+                    int sum = 0;
+
+            }
+                
+
+        }
+    }
+}
