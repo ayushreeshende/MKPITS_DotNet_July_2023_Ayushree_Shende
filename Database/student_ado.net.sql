04ea82e041836790fb75c3c4f9653f862f955e22
ayushreeshende <ayushreeshede03@gmail.com>
2024-01-04 22:31:39 +0530

    ado.net

---
+++ Database/student_ado.net.sql
@@ -0,0 +1,10 @@
+create table student122(
+first_name varchar(20)not null,
+last_name varchar(20)not null,
+city varchar(20)not null,
+state varchar(20)not null);
+
+insert into student122 values('Ayushree','Shende','nagpur','maharashtra')
+insert into student122 values('shruti','meshram','pune','up')
+insert into student122 values('kuku','dunki','mumbai','rajasthan')
+select * from student122
