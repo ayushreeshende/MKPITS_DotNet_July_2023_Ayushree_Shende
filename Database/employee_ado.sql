87cb2742cacb684a6cda92baa5f1fd01891b4aa2
ayushreeshende <ayushreeshede03@gmail.com>
2024-01-06 22:52:45 +0530

    ado.net

---
+++ Database/employee_ado.sql
@@ -0,0 +1,11 @@
+create table employee1(
+empid int primary key,
+empname varchar(20)not null,
+dob varchar(20),
+age int,
+city varchar (20),
+mobno varchar (20),
+salary varchar(20));
+
+select * from employee1
+drop table employee1
