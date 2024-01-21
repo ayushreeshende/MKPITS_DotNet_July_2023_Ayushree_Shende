444dc5bcbb28a7e2e68fe79e768f14f949efdfe4
ayushreeshende <ayushreeshede03@gmail.com>
2024-01-21 13:27:32 +0530

    asp.net

---
+++ Asp.net/area_of_circle.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace area_of_circle
+{
+    public partial class area_of_circle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+       
+            
+
+           int radius = Convert.ToInt32(TextBox1.Text);
+            float area;
+
+            area = 3.14f * radius * radius;
+            Label2.Text = area.ToString();
+            
+            
+
+
+
+
+        }
+    }
+}
